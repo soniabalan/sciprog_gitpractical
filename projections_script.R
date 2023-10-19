6d80a1638f8a01e608f749961246743a4d3930ce
soniabalan <soniaa.balan@gmail.com>
2023-10-19 12:24:44 +0200

    Update projections_script.R

--- projections_script.R
+++ projections_script.R
@@ -1,6 +1,6 @@
 # install.packages("readxl")
 library(readxl)
-source("Mean_centered_function.R","autoscaling.R","paretoscaling.R","pca.R",) #add script names
+source("autoscaling.R") #add script names
 
 # load data
 data <- read_excel("Wine.xlsx", col_names = F)
@@ -9,13 +9,14 @@ colnames(data)[ncol(data)] <- "class"
 # select training data
 data_train <- data[data$class < 3,]
 data_test <- data[data$class == 3,]
-data_train_scaled <- autoscaling(data_train)
-`,data_test_mean, data_test_std <- autoscaling(data_test)
-data_test_scaled <- autoscaling(data_test, data_test_mean, data_test_std)
+data_train_scaled <- autoScaling(data_train)
+data_test_mean <- colMeans(data_train)
+data_test_std <- sapply(data, sd)
+data_test_scaled <- autoScaling(data_test, data_test_mean, data_test_std)
 
 # also save mean and std
 
-scores,loadings <- pca(data_train_scaled)
-scores_projected <- data_test_scaled * loadings
-
-visualization(c[scores;scores_projected])
+# scores,loadings <- pca(data_train_scaled)
+# scores_projected <- data_test_scaled * loadings
+# 
+# visualization(c[scores;scores_projected])
