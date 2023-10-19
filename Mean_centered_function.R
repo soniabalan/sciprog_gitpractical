5996913a619845b688aa1ad5f2fd373131ffe403
MelleKlaver <115146476+MelleKlaver@users.noreply.github.com>
2023-10-19 12:17:50 +0200

    Update Mean_centered_function.R

--- Mean_centered_function.R
+++ Mean_centered_function.R
@@ -1,9 +1,13 @@
 library("readxl")
 
-setwd("C:/Users/MK122/OneDrive/Bureaublad/School/Systems Biology/Scientific programming/Final skills session")
+setwd("C:/Users/MK122/OneDrive/Bureaublad/School/Systems Biology/Scientific programming/Final skills session/Git/sciprog_gitpractical")
 Wine = read_excel("Wine.xlsx", col_names = FALSE)
 mean_centering <- function(data) {
   Mean_vector <- colMeans(data)
   Mean_centered_data <- sweep(data, 2, Mean_vector, "-")
-  return(Mean_centered_data)
+  return(list(Mean_centered_data = Mean_centered_data, Mean_vector = Mean_vector))
 }
+
+Scaled_data = mean_centering(Wine)
+Mean_centered_data = Scaled_data$Mean_centered_data
+Mean_vector = Scaled_data$Mean_vector
