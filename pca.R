d20881c2fab4c6d4d297bc4ad8ffd86277de4a68
luca <lukfari@gmail.com>
2023-10-19 11:42:28 +0200

    just modified pca file

--- pca.R
+++ pca.R
@@ -2,6 +2,10 @@
 library("readxl")
 Wine <- read_excel("Wine.xlsx", col_names = FALSE)
 
+principal_comp <- prcomp(Wine, scale. = FALSE)
+
+plot(principal_comp$x[, 1], principal_comp$x[, 2], main = "PCA", xlab = "PC1", ylab = "PC2")
+
 
 
 
