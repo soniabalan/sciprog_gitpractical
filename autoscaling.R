15325d2d4a39a2d86f2b23cb5ae97f1be824b1aa
Diego Rodriguez <poyutero@gmail.com>
2023-10-19 12:16:41 +0200

    allow for input mean and std

--- autoscaling.R
+++ autoscaling.R
@@ -8,9 +8,19 @@
 ## Data ----
 
 # Functions ----
-autoScaling = function(data){
-  col_means = colMeans(data)
-  col_sd = sapply(data, sd)
+autoScaling = function(data, mean_in = NULL, std_in = NULL){
+  if (!is.null(mean_in)){
+    col_means = colMeans(data)
+  }else{
+    col_means = mean_in
+  }
+  
+  if (!is.null(mean_in)){
+    col_sd = sapply(data, sd)
+  }else{
+    col_sd = std_in
+  }
+  
   data_mean_scaled = data - replicate(nrow(data), col_means)
   data_mean_scaled = data_means_scaled / replicate(nrow(data), col_sd)
   return(data_mean_scaled)
