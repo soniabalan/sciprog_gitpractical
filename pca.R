c8c3097978953fe634d762133858d90607dc560f
luca <lukfari@gmail.com>
2023-10-19 12:13:52 +0200

    created a function to perform PCA and do a plot

--- pca.R
+++ pca.R
@@ -1,11 +1,7 @@
 
-library("readxl")
-Wine <- read_excel("Wine.xlsx", col_names = FALSE)
 
-principal_comp <- prcomp(Wine, scale. = FALSE)
-
-plot(principal_comp$x[, 1], principal_comp$x[, 2], main = "PCA", xlab = "PC1", ylab = "PC2")
-
-
-
-
+# data methos scaled is th sale wine file and in classes you have row associated to their labels 
+pca <- function(data_method_scaled,classes) { 
+  principal_comp <- prcomp(data_method_scaled, scale. = FALSE)
+  plot(principal_comp$x[, 1], principal_comp$x[, 2], main = "PCA",col = classes, xlab = "PC1", ylab = "PC2")
+}
